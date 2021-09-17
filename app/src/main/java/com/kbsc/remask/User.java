312696a2153da312bcdbccc3a23f3e5de7b45ca6
a-taeyeon <taeyeon0107@naver.com>
2021-09-18 03:13:41 +0900

    signup, login

---
+++ app/src/main/java/com/kbsc/remask/User.java
@@ -0,0 +1,73 @@
+package com.kbsc.remask;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class User {
+    private String userName;
+    private String email;
+    private String password;
+    private String phone;
+    private boolean seller;
+
+    public User(){}
+
+    public User(String userName, String email, String password, String phone, boolean seller) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.seller = seller;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean getSeller() {
+        return seller;
+    }
+
+    public void setSeller(boolean seller) {
+        this.seller = seller;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                ", seller=" + seller +
+                '}';
+    }
+}
